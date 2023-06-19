98cf6f8de5e9bec294a3ce13ff508c2aa59857ad
Wyatt <parsonswy@gmail.com>
2023-06-19 19:34:26 -0400

    cleanup map

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -5,13 +5,26 @@ import PCNLogo from '@/assets/images/PCNLogo.png';
 
 import '@/css/components/SidePanel.css';
 
+class PanelLink {
+	view(vnode) {
+		return m('li.side_panel__body-link', {
+			class: [
+				`${m.route.param('map').toUpperCase() == vnode.attrs.map.toUpperCase() ? 'side-panel__body-link--selected': ''}`
+			],
+			onclick: () => { m.route.set('/:map', { map: vnode.attrs.map.toLowerCase() }); },
+		}, [
+			m('div', vnode.attrs.alias || vnode.attrs.map),
+			m('div', { style: 'font-size: 10px' }, vnode.attrs.when || '—')
+		]);
+	}
+}
+
 export default class SidePanel {
 	oninit() {
 		this.expanded = true;
 	}
 	
 	view() {
-		console.log(m.route.param('map').toUpperCase() == 'AINCRAD');
 		return m('.side-panel', {
 			class: [
 				`${this.expanded? 'side-panel--expanded' : ''}`
@@ -29,31 +42,14 @@ export default class SidePanel {
 					}),
 				]),
 				m('ul.side-panel__body-links', [
-					m('li.side_panel__body-link', {
-						class: [
-							`${m.route.param('map').toUpperCase() == 'AINCRAD'? 'side-panel__body-link--selected': ''}`
-						],
-						onclick: () => { m.route.set('/:map', { map: 'aincrad'}); },
-					}, 'Aincrad'),
-					m('li.side_panel__body-link', {
-						class: [
-							`${m.route.param('map').toUpperCase() == 'CREATIVE'? 'side-panel__body-link--selected': ''}`
-						],
-						onclick: () => { m.route.set('/:map', { map: 'creative'}); },
-					}, 'Creative'),
-					/* m('li.side_panel__body-link', {
-						class: [
-							`${m.route.param('map').toUpperCase() == 'LOBBY'? 'side-panel__body-link--selected': ''}`
-						],
-						onclick: () => { m.route.set('/:map', { map: 'lobby'}); },
-					}, 'Lobby'),
-					m('li.side_panel__body-link', {
-						class: [
-							`${m.route.param('map').toUpperCase() == 'PVE'? 'side-panel__body-link--selected': ''}`
-						],
-						onclick: () => { m.route.set('/:map', { map: 'pve'}); },
-					}, 'PvE'),*/
-				])
+					m(PanelLink, { map: 'Aincrad', }),
+					m(PanelLink, { map: 'Creative', when: 'July 2019 - December 2022' }),
+					m(PanelLink, { map: 'Lobby', when: 'May 22, 2019 - December 2022' }),
+					// m(PanelLink, { map: '2022lts', alias: 'Long Term Survival (LTS)', when: 'December 2021 - December 2022' }),
+					m(PanelLink, { map: '2019lts', alias: 'Long Term Survival (LTS/PvE)', when: 'January 2019 - December 2021' }),
+					m(PanelLink, { map: 'Trench', when: 'October 2019 - December 2022' }),
+				]),
+				m('div.side-panel__footer', m('a.side-panel__footer-link', { href: 'https://goodbye.peacefulcraft.net' }, 'Map Downloads'))
 			]),
 			m('img.side-panel__expander-icon',  {
 				src: IconSideBarExpand,
